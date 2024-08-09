31fe68f15c7251549d1919680db565155f95f2c0
Will-Montero <monteromora99@gmail.com>
2024-08-09 15:16:27 -0500

    arpetas enlazadas

---
+++ src/AppPeliculas.jsx
@@ -0,0 +1,6 @@
+
+export const AppPeliculas = () => {
+  return (
+    <div>AppPeliculas</div>
+  )
+}
---
+++ src/hook/fetchData.js
@@ -0,0 +1,6 @@
+
+export const fetchData = () => {
+  return (
+    <div>fetchData</div>
+  )
+}
