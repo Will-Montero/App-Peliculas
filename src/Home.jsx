ac615cae2d7390baed3ebe44f5dc79a42fbe2590
Will-Montero <monteromora99@gmail.com>
2024-08-13 17:15:14 -0500

    celulares ya home

--- src/Home.jsx
+++ src/Home.jsx
@@ -8,47 +8,92 @@ export const Home = () => {
         <div className="pelicula-card">
         <img src={laAbuela}
         loading='lazy' className="imagen-card"/>
+        <div className='info-peli'>
         <h3 className="titulo-card">La abuela</h3>
+        <p className='info-peli-año'>2021</p>
+        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        </div>
         </div>
         <div className="pelicula-card">
         <img src={laAbuela}
         loading='lazy' className="imagen-card"/>
-        <h3 className="titulo-card"></h3>
+        <div className='info-peli'>
+        <h3 className="titulo-card">La abuela</h3>
+        <p className='info-peli-año'>2021</p>
+        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        </div>
         </div>
         <div className="pelicula-card">
         <img src={laAbuela}
         loading='lazy' className="imagen-card"/>
-        <h3 className="titulo-card"></h3>
+        <div className='info-peli'>
+        <h3 className="titulo-card">La abuela</h3>
+        <p className='info-peli-año'>2021</p>
+        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        </div>
         </div>
         <div className="pelicula-card">
         <img src={laAbuela}
         loading='lazy' className="imagen-card"/>
-        <h3 className="titulo-card"></h3>
+        <div className='info-peli'>
+        <h3 className="titulo-card">La abuela</h3>
+        <p className='info-peli-año'>2021</p>
+        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        </div>
         </div>
         <div className="pelicula-card">
         <img src={laAbuela}
         loading='lazy' className="imagen-card"/>
-        <h3 className="titulo-card"></h3>
+        <div className='info-peli'>
+        <h3 className="titulo-card">La abuela</h3>
+        <p className='info-peli-año'>2021</p>
+        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        </div>
         </div>
         <div className="pelicula-card">
         <img src={laAbuela}
         loading='lazy' className="imagen-card"/>
-        <h3 className="titulo-card"></h3>
+        <div className='info-peli'>
+        <h3 className="titulo-card">La abuela</h3>
+        <p className='info-peli-año'>2021</p>
+        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        </div>
         </div>
         <div className="pelicula-card">
         <img src={laAbuela}
         loading='lazy' className="imagen-card"/>
-        <h3 className="titulo-card"></h3>
+        <div className='info-peli'>
+        <h3 className="titulo-card">La abuela</h3>
+        <p className='info-peli-año'>2021</p>
+        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        </div>
         </div>
         <div className="pelicula-card">
         <img src={laAbuela}
         loading='lazy' className="imagen-card"/>
-        <h3 className="titulo-card"></h3>
+        <div className='info-peli'>
+        <p className="titulo-card">La abuela</p>
+        <p className='info-peli-año'>2021</p>
+        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        </div>
         </div>
         <div className="pelicula-card">
         <img src={laAbuela}
         loading='lazy' className="imagen-card"/>
-        <h3 className="titulo-card"></h3>
+        <div className='info-peli'>
+        <h3 className="titulo-card">La abuela</h3>
+        <p className='info-peli-año'>2021</p>
+        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        </div>
+        </div>
+        <div className="pelicula-card">
+        <img src={laAbuela}
+        loading='lazy' className="imagen-card"/>
+        <div className='info-peli'>
+        <h3 className="titulo-card">La abuela</h3>
+        <p className='info-peli-año'>2021</p>
+        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        </div>
         </div>
     </div>
   </section>
