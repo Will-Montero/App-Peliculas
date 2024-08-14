df6ffba209421c9fd5662be9935f81458a070537
Will-Montero <monteromora99@gmail.com>
2024-08-13 22:06:08 -0500

    home

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,6 +2,10 @@ import './styles/home.css'
 import  laAbuela from './assets/homePeliculas/laAbuela.webp'
 import aFondo from './assets/homePeliculas/aFondo.jpg'
 import niebla from './assets/homePeliculas/niebla.jpg'
+import rapidos from './assets/homePeliculas/rapidos.jpg'
+import hitman from './assets/homePeliculas/hitman.jpg'
+import terminator from './assets/homePeliculas/terminator.jpg'
+import pasajeros from './assets/homePeliculas/pasajeros.jpg'
 export const Home = () => {
   return (
   <>
@@ -35,39 +39,39 @@ export const Home = () => {
         </div>
         </div>
         <div className="pelicula-card">
-        <img src={laAbuela}
+        <img src={rapidos}
         loading='lazy' className="imagen-card"/>
         <div className='info-peli'>
-        <h3 className="titulo-card">La abuela</h3>
-        <p className='info-peli-año'>2021</p>
-        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        <h3 className="titulo-card">Rápidos y furiosos 7 </h3>
+        <p className='info-peli-año'>2015</p>
+        <p className='info-peli-resumen'>Dominic Toretto, Brian OConner y los demás regresan a Estados Unidos después de derrotar a Owen Shaw y su pandilla para intentar vivir una vida normal. </p>
         </div>
         </div>
         <div className="pelicula-card">
-        <img src={laAbuela}
+        <img src={hitman}
         loading='lazy' className="imagen-card"/>
         <div className='info-peli'>
-        <h3 className="titulo-card">La abuela</h3>
-        <p className='info-peli-año'>2021</p>
-        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        <h3 className="titulo-card">Hitman agente 47</h3>
+        <p className='info-peli-año'>2007</p>
+        <p className='info-peli-resumen'>Hit Man es una película estadounidense de 2023, dirigida por Richard Linklater sobre un guion del propio Linklater y Glen Powell, y protagonizada por este ... </p>
         </div>
         </div>
         <div className="pelicula-card">
-        <img src={laAbuela}
+        <img src={terminator}
         loading='lazy' className="imagen-card"/>
         <div className='info-peli'>
-        <h3 className="titulo-card">La abuela</h3>
-        <p className='info-peli-año'>2021</p>
-        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        <h3 className="titulo-card">Terminator 2</h3>
+        <p className='info-peli-año'>1991</p>
+        <p className='info-peli-resumen'>Dos exterminadores llegan del futuro para rastrear al joven hijo de Sarah Connor, John. Uno de los cíborgs está programado para eliminarlo; el otro, ... </p>
         </div>
         </div>
         <div className="pelicula-card">
-        <img src={laAbuela}
+        <img src={pasajeros}
         loading='lazy' className="imagen-card"/>
         <div className='info-peli'>
-        <h3 className="titulo-card">La abuela</h3>
-        <p className='info-peli-año'>2021</p>
-        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        <h3 className="titulo-card">Passengers</h3>
+        <p className='info-peli-año'>2016</p>
+        <p className='info-peli-resumen'>Una nave espacial que viaja a un planeta-colonia lejano y transporta a miles de personas, tiene una avería en una de sus cámaras de sueño.</p>
         </div>
         </div>
         <div className="pelicula-card">
@@ -75,15 +79,6 @@ export const Home = () => {
         loading='lazy' className="imagen-card"/>
         <div className='info-peli'>
         <p className="titulo-card">La abuela</p>
-        <p className='info-peli-año'>2021</p>
-        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
-        </div>
-        </div>
-        <div className="pelicula-card">
-        <img src={laAbuela}
-        loading='lazy' className="imagen-card"/>
-        <div className='info-peli'>
-        <h3 className="titulo-card">La abuela</h3>
         <p className='info-peli-año'>2021</p>
         <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
         </div>
