77b3023e337f9151f89b278f7be33749836e5197
Will-Montero <monteromora99@gmail.com>
2024-08-13 21:52:42 -0500

    peliculas de home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,5 +1,7 @@
 import './styles/home.css'
 import  laAbuela from './assets/homePeliculas/laAbuela.webp'
+import aFondo from './assets/homePeliculas/aFondo.jpg'
+import niebla from './assets/homePeliculas/niebla.jpg'
 export const Home = () => {
   return (
   <>
@@ -15,21 +17,21 @@ export const Home = () => {
         </div>
         </div>
         <div className="pelicula-card">
-        <img src={laAbuela}
+        <img src={aFondo}
         loading='lazy' className="imagen-card"/>
         <div className='info-peli'>
-        <h3 className="titulo-card">La abuela</h3>
-        <p className='info-peli-año'>2021</p>
-        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        <h3 className="titulo-card">A fondo</h3>
+        <p className='info-peli-año'>2017</p>
+        <p className='info-peli-resumen'>Una familia toma su coche rumbo a las vacaciones de verano. El control electrónico de velocidad lo ajustan en 130 km/h. Durante el viaje se da cuenta de que se ha quedado atorada en esa velocidad.  </p>
         </div>
         </div>
         <div className="pelicula-card">
-        <img src={laAbuela}
+        <img src={niebla}
         loading='lazy' className="imagen-card"/>
         <div className='info-peli'>
-        <h3 className="titulo-card">La abuela</h3>
-        <p className='info-peli-año'>2021</p>
-        <p className='info-peli-resumen'>Pilar llega a su apartamento donde encuentra el cuerpo de otra mujer de su misma edad. Después, una joven desnuda se aparece frente a ella y le besa la mano. </p>
+        <h3 className="titulo-card">Niebla sin fronteras</h3>
+        <p className='info-peli-año'>2024</p>
+        <p className='info-peli-resumen'>Una detective de Yakarta investiga una misteriosa ola de asesinatos en la frontera entre Indonesia y Malasia... y termina enfrentándose a los fantasmas de su pasado. </p>
         </div>
         </div>
         <div className="pelicula-card">
