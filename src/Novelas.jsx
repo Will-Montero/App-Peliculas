a2ae8ff2a5087c3b14f24a9beca2c1bd01f7eb23
Will-Montero <monteromora99@gmail.com>
2024-08-14 20:52:11 -0500

    responsivo de celular

--- src/Novelas.jsx
+++ src/Novelas.jsx
@@ -3,7 +3,7 @@ import './styles/novelas.css'
 
 export const Novelas = () => {
 
-    const [dataSeries, setDataSeries] = useState([])
+    const [dataNovelas, setDataNovelas] = useState([])
 
     const URL = "https://api.themoviedb.org/3/discover/tv";
   const API_KEY = "9b99ed6f20e2bfc951d790cf5a420564";
@@ -12,7 +12,7 @@ export const Novelas = () => {
     try {
         const response = await fetch(`${URL}?api_key=${API_KEY}&with_genres=18`);
         const data = await response.json()
-        setDataSeries(data.results)
+        setDataNovelas(data.results)
     } catch (error) {
         console.log(error)
     }
@@ -33,7 +33,7 @@ export const Novelas = () => {
     <div className="container-novelas">
       <h1 className='novela-titulo'>Novelas Populares ▶️</h1>
       <div className="novela-list">
-        {dataSeries.map((novela) => (
+        {dataNovelas.map((novela) => (
           <div key={novela.id} className="novela-card">
             <div className="novela-info">
                 <span className="novela-year">
@@ -46,7 +46,10 @@ export const Novelas = () => {
               className="novela-poster"
             />
             <div className="novela-details">
-            <p className="novela-year">{novela.first_air_date.split("-")[0]}</p>
+            <p className='novela-titulo-details'>
+                {limitarPalabras(novela.name, 3)}
+            </p>
+            <p className="novela-year-details">{novela.first_air_date.split("-")[0]}</p>
               <p className="novela-overview">{limitarPalabras(novela.overview, 23)}</p>
             </div>
           </div>
