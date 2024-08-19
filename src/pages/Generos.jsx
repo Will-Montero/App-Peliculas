bed01681eb7101dd19e879e112ed9201c4a95904
Will-Montero <monteromora99@gmail.com>
2024-08-19 14:58:00 -0500

    classname

--- src/pages/Generos.jsx
+++ src/pages/Generos.jsx
@@ -6,16 +6,16 @@ export const Generos = ({ genero }) => {
   const { generos } = useContext(GenerosContext);
 
   return (
-    <div clas>
-      <h2>{genero}</h2>
-      <div>
+    <div className="genero-container">
+      <h2 className="genero-titulo-card">{genero}</h2>
+      <div className="genero-">
         {generos[genero] ? (
             
           generos[genero].map((movie) => (
             
             <div key={movie.id}>
-              <h3>{movie.title}</h3>
-              <img
+              <h3 className="genero-titulo-info">{movie.title}</h3>
+              <img className="genero-poster"
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
               />
