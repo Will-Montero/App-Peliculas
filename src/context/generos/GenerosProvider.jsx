90955f2e008e99d00cc8ec7c94ebbea8995efaa0
Will-Montero <monteromora99@gmail.com>
2024-08-19 13:06:50 -0500

    fetch

--- src/context/generos/GenerosProvider.jsx
+++ src/context/generos/GenerosProvider.jsx
@@ -1,7 +1,23 @@
 import { useState, useEffect } from "react"
 
+export const GenerosProvider = ({ children }) => {
+    const [generos, setGeneros] = useState({})
 
-export const GenerosProvider = () => {
+    const fetchGeneros = async () => {
+        const generosIds = {
+            Action: 28,
+        Adventure: 12,
+        Drama: 18,
+        Horror: 27,
+        "Science Fiction": 878,
+        Animation: 16,
+        History: 36
+        }
+    }
+
+    const fetchGenero = async ( genero ) => ñ
+    const response = await  fetch (`https://api.themoviedb.org/3/discover/movie?api_key=<<YOUR_API_KEY>>&with_genres=${genreIds[genre]}`)
+
   return (
     <>
     
