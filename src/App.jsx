ec4faa1f259c26b0985ac9bda19007cee8df7ee7
Will-Montero <monteromora99@gmail.com>
2024-08-19 13:30:33 -0500

    generos jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import { Search } from "./components/Search"
 import { Routes, Route, Navigate } from "react-router-dom";
 import './styles/App.css'
 import { SearchProvider } from "./context/pelicula/SearchProvider"
+import GenrePage from './Ge'
 
 export const App = () => {
   return (
@@ -20,6 +21,13 @@ export const App = () => {
       <Route path="/series" element={<Series></Series>}></Route>
       <Route path="/novelas" element={<Novelas></Novelas>}></Route>
       <Route path="/*" element={<Navigate to='/'/>}></Route>
+      <Route path="/accion" element={<GenrePage genre="Action" />} />
+          <Route path="/aventura" element={<GenrePage genre="Adventure" />} />
+          <Route path="/drama" element={<GenrePage genre="Drama" />} />
+          <Route path="/horror" element={<GenrePage genre="Horror" />} />
+          <Route path="/ciencia-ficcion" element={<GenrePage genre="Science Fiction" />} />
+          <Route path="/animacion" element={<GenrePage genre="Animation" />} />
+          <Route path="/historia" element={<GenrePage genre="History" />} />
     </Routes>
     
     <Footer></Footer>
---
+++ src/pages/Generos.jsx
@@ -0,0 +1,27 @@
+import { useContext } from "react";
+import { GenresContext } from "./GenresContext";
+
+export const GenerosPage = ({ genero }) => {
+  const { generos } = useContext(GenresContext);
+
+  return (
+    <div>
+      <h2>{genero}</h2>
+      <div>
+        {generos[genero] ? (
+          generos[genero].map((movie) => (
+            <div key={movie.id}>
+              <h3>{movie.title}</h3>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+              />
+            </div>
+          ))
+        ) : (
+          <p>Cargando películas...</p>
+        )}
+      </div>
+    </div>
+  );
+};
