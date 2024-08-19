e57c80dc3f7d8667ceeb6c74d67d63074abab8a4
Will-Montero <monteromora99@gmail.com>
2024-08-19 13:03:33 -0500

    hooks

--- src/context/generos/GenerosProvider.jsx
+++ src/context/generos/GenerosProvider.jsx
@@ -1,7 +1,10 @@
-import React from 'react'
+import { useState, useEffect } from "react"
+
 
 export const GenerosProvider = () => {
   return (
-    <div>GenerosProvider</div>
+    <>
+    
+    </>
   )
 }
