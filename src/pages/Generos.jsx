6fa7a7f009626586b6ab74a71e192d53c4a804fc
Will-Montero <monteromora99@gmail.com>
2024-08-19 15:12:03 -0500

    css de generos terminado

--- src/pages/Generos.jsx
+++ src/pages/Generos.jsx
@@ -1,27 +1,49 @@
 import { useContext } from "react";
 import { GenerosContext } from "../context/generos/GenerosContext";
-import '../styles/generos.css'
+import "../styles/generos.css";
 
 export const Generos = ({ genero }) => {
   const { generos } = useContext(GenerosContext);
 
+  const limitarPalabras = (texto, maxPalabras) => {
+    const palabras = texto.split(" ");
+    if (palabras.length > maxPalabras) {
+      return palabras.slice(0, maxPalabras).join(" ") + "...";
+    }
+    return texto;
+  };
+
   return (
     <div className="genero-container">
-      <h2 className="genero-titulo-card">{genero}</h2>
-      <div className="genero-">
+      <h2 className="genero-titulo">{genero}</h2>
+      <div className="genero-list">
         {generos[genero] ? (
-            
           generos[genero].map((movie) => (
-            
-            <div key={movie.id}>
-              <h3 className="genero-titulo-info">{movie.title}</h3>
-              <img className="genero-poster"
+            <div className="genero-card" key={movie.id}>
+              <div className="genero-info">
+                <span className="genero-year">
+                  {movie.release_date.split("-")[0]}
+                </span>
+              </div>
+              <img
+                className="genero-poster"
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
               />
+
+              <div className="genero-details">
+                <h2 className="genero-title">
+                  {limitarPalabras(movie.title, 4)}
+                </h2>
+                <p className="genero-year-details">
+                  {movie.release_date.split("-")[0]}
+                </p>
+                <p className="genero-overview">
+                  {limitarPalabras(movie.overview, 27)}
+                </p>
+              </div>
             </div>
           ))
-          
         ) : (
           <p>Cargando películas...</p>
         )}
