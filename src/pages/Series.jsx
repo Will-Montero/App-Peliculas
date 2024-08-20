fe1282083a0974c53623c2d30a0a7e445ed5f588
Will-Montero <monteromora99@gmail.com>
2024-08-19 19:33:54 -0500

    css de series optimizado

--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -62,7 +62,7 @@ export const Series = () => {
                 <p className="series-overview">
                   {limitarPalabras(serie.overview, 25)}
                 </p>
-                <NavLink to={`/reproducir/${serie.id}`} className="play-button">
+                <NavLink to={`/reproducir/${serie.id}`} className="play-button-series">
                   Reproducir
                 </NavLink>
               </div>
