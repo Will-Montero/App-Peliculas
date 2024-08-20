2dfd6d29c2d523b7b697be9d9db8ea8359176b16
Will-Montero <monteromora99@gmail.com>
2024-08-19 23:10:53 -0500

    aviso

--- src/pages/ReproducirPelicula.jsx
+++ src/pages/ReproducirPelicula.jsx
@@ -25,9 +25,9 @@ export const ReproducirPelicula = () => {
       <div className="video-reproductor">
         
         <img className="imagen-reproductor" src={avisoEnsayo} alt="error" />
-        <p className="aviso-reproductor"></p>
+
       </div>
-      <p className="overview-reproductor">Lamentamos no traer la pelicula, PelisFree es una App de ensayo en el cual se hacen un fetch de una api: TMBD y me ayuda a practicar React</p>
+      <p className="overview-reproductor">Lamentamos no traer la pelicula, PelisFree es una App de ensayo en el cual se hacen un fetch de una api: TMBD el cual me ayuda a practicar React</p>
     </div>
   );
 };
