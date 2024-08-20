fcced9287dabfbd5b8b122013c234425c280d888
Will-Montero <monteromora99@gmail.com>
2024-08-19 21:56:32 -0500

    img y componente

---
+++ src/components/Error.jsx
@@ -0,0 +1,8 @@
+import noHayVideo from '../assets/error/noHayVideo.png'
+export const Error = () => {
+  return (
+    <main>
+        <img src={noHayVideo} alt="No hay video" />
+    </main>
+  )
+}
